b4aaf255ffca043d3c88e877e7f1d2eb0518434f
UrangBanua <afni.afdillah@live.com>
2023-06-30 16:07:54 +0800

    add session cookies to baypass login

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 import json
 import locale
+import pickle
 import getpass
 import requests
 import pandas as pd
@@ -11,7 +12,20 @@ from colorama import Fore, Style
 # Set lokalisasi atau regional ke Indonesia
 locale.setlocale(locale.LC_ALL, 'id_ID')
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ fungsi loadingPage ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~ Fungsi untuk menyimpan session cookies ke file ~~~~~~~~~~~~~~~~~~~~
+def save_cookies(session, filename):
+    with open(filename, 'wb') as f:
+        pickle.dump(session.cookies, f)
+
+# ~~~~~~~~~~~~~~~~~~~~ Fungsi untuk memuat session cookies dari file ~~~~~~~~~~~~~~~~~~~~
+def load_cookies(session, filename):
+    try:
+        with open(filename, 'rb') as f:
+            session.cookies.update(pickle.load(f))
+    except FileNotFoundError:
+        print(Fore.RED + '! File Session tidak ditemukan' + Style.RESET_ALL)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ fungsi loadingPage ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def loadingPage(caption):
     # Mengambil panjang konten respons
     content_length = response.headers.get('content-length')
@@ -27,25 +41,20 @@ def loadingPage(caption):
             pbar.set_postfix_str('waiting response...')
             pbar.update(len(chunk))
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ fungsi logout ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ fungsi logout ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def logout():    
     response = session.post(urlserver + '/logout')
     loadingPage('Logout... ')
 
 # Buat session
+print(Fore.BLUE + '~ memuat sesi awal' + Style.RESET_ALL)
 session = requests.Session()
 
-# Bersihkan session
-session.cookies.clear()
-print(Fore.BLUE + '~ memuat sesi awal' + Style.RESET_ALL)
+# Muat cookies jika ada
+load_cookies(session, 'cookies.pkl')
 
-# promt input username & password
+# promt input parameter server login
 urlserver = input('UrlServer :(default FMIS Kab. HST) ') or 'https://hulusungaitengahkab.fmis.id/2023'
-kduser = input('Username  :(default akuntansiBPKAD) ') or 'akuntansiBPKAD'
-password = getpass.getpass('Password  : ')
-while not password:
-    print(Fore.RED + '! Password tidak boleh kosong' + Style.RESET_ALL)
-    password = getpass.getpass('Password  : ')
 
 # Mengirim GET request ke halaman sebelum login
 response = session.get(urlserver)
@@ -57,40 +66,58 @@ soup = BeautifulSoup(response.content, 'html.parser')
 token = soup.find('input', {'name': '_token'})['value']
 print(Fore.GREEN + '~ token didapatkan:', token + ' \n' + Style.RESET_ALL)
 
-# URL halaman login
-login_url = urlserver + '/login'
+# Pengecekan apakah sudah langsung terpindah ke halaman dashboard
+print(Fore.BLUE + '~ cek session apakah expired' + Style.RESET_ALL)
+if response.status_code == 200 and '/dashboard' in response.url:
+    print(Fore.GREEN + 'Halaman langsung terpindah ke Dashboard' + Style.RESET_ALL)
+    # tambah code selanjutnya
 
-# Data login (sesuaikan dengan field input pada halaman login)
-payload = {
-    '_token': token,
-    'kduser': kduser,
-    'password': password,
-    'tahun': '2023'
-}
-
-# Header Content-Type
-headers = {
-    'Content-Type': 'application/x-www-form-urlencoded'
-}
-
-print(Fore.BLUE + '~ mulai coba login' + Style.RESET_ALL)
-
-# Mengirim POST request untuk login
-response = session.post(login_url, data=payload, headers=headers, stream=True)
-loadingPage('Logging in ')
-
-# Cek apakah login berhasil
-if response.status_code == 200:
-    print(Fore.GREEN + 'Login berhasil' + Style.RESET_ALL,' :D\n')
 else:
-    print(Fore.RED + '! Login gagal' + Style.RESET_ALL)
-    print('! Respon HTTP:', response.status_code)
-    print('! Pesan Kesalahan:', response.text)
+    print(Fore.BLUE + '~ session sudah expired, login dulu !' + Style.RESET_ALL)
+
+    # promt input parameter server login
+    kduser = input('Username  :(default akuntansiBPKAD) ') or 'akuntansiBPKAD'
+    password = getpass.getpass('Password  : ')
+    while not password:
+        print(Fore.RED + '! Password tidak boleh kosong' + Style.RESET_ALL)
+        password = getpass.getpass('Password  : ')
+
+    # URL halaman login
+    login_url = urlserver + '/login'
+    # Data login (sesuaikan dengan field input pada halaman login)
+    payload_login = {
+        '_token': token,
+        'kduser': kduser,
+        'password': password,
+        'tahun': '2023'
+    }
+    # Header login
+    headers_login = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    # Header Json
+    headers_json = {
+        'Accept: application/json'
+        'Connection: keep-alive'
+        'Sec-Fetch-Mode: cors'
+        'Sec-Fetch-Site: same-origin'
+    }
+    print(Fore.BLUE + '~ mulai coba login' + Style.RESET_ALL)
+    # Mengirim POST request untuk login
+    response = session.post(login_url, data=payload_login, headers=headers_login, stream=True)
+    loadingPage('Logging in ')
+    # Cek apakah login berhasil
+    if response.status_code == 200:
+        print(Fore.GREEN + 'Login berhasil' + Style.RESET_ALL,' :D\n')
+        save_cookies(session, 'cookies.pkl')
+    else:
+        print(Fore.RED + '! Login gagal' + Style.RESET_ALL)
+        print('! Respon HTTP:', response.status_code)
+        print('! Pesan Kesalahan:', response.text)
+
 
 # Menampilkan hasil scraping
 print(Fore.BLUE + '** Selamat Datang akuntansiBPKAD **\n'  + Style.RESET_ALL)
-
-
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~ print Proporsi Anggaran Terhadap Realisasi ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 print(Fore.BLUE + '~ mulai coba ambil data Anggaran' + Style.RESET_ALL)
@@ -147,4 +174,4 @@ except Exception as e:
 
 finally:
     print(Fore.LIGHTMAGENTA_EX + 'Bye...' + Style.RESET_ALL)
-    logout()
+    #logout()
